6b00b4d3be53ccb48d25baf012c9852001dade88
Mikita Havryliuk <chinter.games1@gmail.com>
2023-04-14 19:39:28 +0300

    some fixes

--- src/components/Header/Navigation/Navigation.component.tsx
+++ src/components/Header/Navigation/Navigation.component.tsx
@@ -3,10 +3,10 @@ import "./Navigation.component.css";
 export const Navigation = () => {
   const navItems: string[] = ["Clothes", "Sneakers", "Bags", "Accessorize"];
   return (
-    <nav className="header__navigation navigation">
+    <nav className="navigation">
       <ul className="navigation__list">
         {navItems.map((itemName) => (
-          <li className="navigation__item">
+          <li key={itemName} className="navigation__item">
             <a className="navigation__link" href="#">
               {itemName}
             </a>
